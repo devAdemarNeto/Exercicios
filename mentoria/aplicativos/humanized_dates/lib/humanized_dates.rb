951245f31015ed1240e1efdb352458d01ea4b86b
Ademar Neto <ademarnetodev@gmail.com>
2025-02-19 15:29:40 -0300

    creat: humanized_dates

---
+++ mentoria/aplicativos/humanized_dates/lib/humanized_dates.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "humanized_dates/version"
+
+module HumanizedDates
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ mentoria/aplicativos/humanized_dates/spec/humanized_dates_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe HumanizedDates do
+  it "has a version number" do
+    expect(HumanizedDates::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(true).to eq(true)
+  end
+end
