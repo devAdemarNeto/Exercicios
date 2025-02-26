6b6b71e8f6bb1c42db5c556d88b645056cc4479d
Ademar Neto <ademarnetodev@gmail.com>
2025-02-26 04:59:59 -0300

    Implementação inicial do método de conversão no CurrencyConverter

--- mentoria/aplicativos/currency_converter/lib/currency_converter.rb
+++ mentoria/aplicativos/currency_converter/lib/currency_converter.rb
@@ -2,7 +2,33 @@
 
 require_relative "currency_converter/version"
 
-module CurrencyConverter
+module CurrencyConverterModule
   class Error < StandardError; end
-  # Your code goes here...
+  class CurrencyConverter
+    TAXAS_FIXAS = {
+      "USD_TO_BRL" => 5.0,
+      "USD_TO_EUR" => 0.92,
+      "USD_TO_BTC" => 0.000016,      
+      "BRL_TO_USD" => 0.20,
+      "BRL_TO_EUR" => 0.18,
+      "BRL_TO_BTC" => 0.0000032,
+      "EUR_TO_USD" => 1.09,
+      "EUR_TO_BRL" => 5.50,
+      "EUR_TO_BTC" => 0.000017,
+      "BTC_TO_USD" => 62500,
+      "BTC_TO_BRL" => 312500,
+      "BTC_TO_EUR" => 58823
+    }
+
+    def self.convert(from, to, amount)
+      key = "#{from.upcase}_TO_#{to.upcase}"
+
+      if TAXAS_FIXAS.key?(key)
+        amount * TAXAS_FIXAS[key]
+      else
+        raise ArgumentError, "Conversão de #{from} para #{to} não suportada."
+      end
+    end
+  end
+
 end
