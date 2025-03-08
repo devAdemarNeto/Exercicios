1a5587ea01e203dbdc55974043cfac40d3ec1da5
Ademar Neto <ademarnetodev@gmail.com>
2025-03-07 23:45:07 -0300

    create: frete_calculator

---
+++ mentoria/aplicativos/frete_calculator/lib/frete_calculator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "frete_calculator/version"
+
+module FreteCalculator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ mentoria/aplicativos/frete_calculator/lib/frete_calculator/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module FreteCalculator
+  VERSION = "0.1.0"
+end
