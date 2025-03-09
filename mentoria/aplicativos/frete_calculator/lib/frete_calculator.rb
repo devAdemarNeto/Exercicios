cf581b5d044d36f8fc15d900905b63317f4dd318
Ademar Neto <ademarnetodev@gmail.com>
2025-03-08 23:42:34 -0300

    feat: criação da logica do frete_calculator

--- mentoria/aplicativos/frete_calculator/lib/frete_calculator.rb
+++ mentoria/aplicativos/frete_calculator/lib/frete_calculator.rb
@@ -3,6 +3,29 @@
 require_relative "frete_calculator/version"
 
 module FreteCalculator
-  class Error < StandardError; end
-  # Your code goes here...
+  class FreteCalculator
+    attr_reader :peso, :volume, :distancia, :transporte
+  
+    # Tabela de preços fictícia
+    PRECO_BASE = {
+      moto: 0.5,    # R$ 0,50 por km
+      carro: 1.0,   # R$ 1,00 por km
+      caminhao: 2.5 # R$ 2,50 por km
+    }
+  
+    def initialize(peso:, volume:, distancia:, transporte: :carro)
+      @peso = peso
+      @volume = volume
+      @distancia = distancia
+      @transporte = transporte
+    end
+  
+    def calcular_frete
+      taxa_transporte = PRECO_BASE[@transporte]
+      raise "Transporte inválido!" unless taxa_transporte
+  
+      custo = (@distancia * taxa_transporte) + (peso * 0.1) + (volume * 0.05)
+      custo.round(2) # Retorna o valor final arredondado
+    end
+  end  
 end
